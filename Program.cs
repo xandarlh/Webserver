54c4d350b91b93e35e8f5f66892ea00817e739e2
Alexandar Lackovic Hansen <alex303a@zbc.dk>
2021-08-11 08:20:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Webserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress iPAddress = IPAddress.Parse("10.108.168.253");
+            Webserver webserver = new Webserver();
+            webserver.start(iPAddress, 80, 2, @"C:\ting");
+            Console.Read();
+        }
+    }
+}
